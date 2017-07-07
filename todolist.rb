b90222082fda3f3d9151b69038619765efcb47ce
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-07 19:29:54 -0400

    finished user stories

--- todolist.rb
+++ todolist.rb
@@ -12,7 +12,7 @@ module Menu
 end
 
 module Promptable
-  def prompt(message - "What would you like to do?", symbol = ":> ")
+  def prompt(message = "What would you like to do?", symbol = ":> ")
     print message
     print symbol
     gets.chomp
@@ -42,6 +42,28 @@ class List
 end
 
 if __FILE__ == $PROGRAM_NAME
+  include Menu
+  include Promptable
+  my_list = List.new
+  puts "Please choose from the following list"
+    until ['q'].include?(user_input = prompt(show).downcase)
+      case user_input
+      when "1" 
+        my_list.add(Task.new(prompt('What is the task you 
+          would like to accomplish?')))
+      when "2"
+        puts my_list.show
+      else
+        puts "Sorry, I did not understand or q to quit"
+      end
+      prompt('Press enter to continue','')
+
+    end
+    puts "Thanks for using the menu system!"
+end
+
+=begin
+if __FILE__ == $PROGRAM_NAME
   my_list = List.new
   puts 'You have created a new list'
   #my_list.add(Task.new('Make Breakfast'))
@@ -51,4 +73,5 @@ if __FILE__ == $PROGRAM_NAME
   puts 'You have added a task to the Todo List'
   puts my_list.show
 end
+=end
 
