60b963b298ea74b16cfeeecab636783cdefa326b
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-07 17:12:36 -0400

    adding classes and methods

--- todolist.rb
+++ todolist.rb
@@ -1,2 +1,26 @@
 class List
+  attr_reader :all_tasks
+  def initialize(all_tasks)
+    @all_tasks = []
+  end
+
+  def add(task)
+    all_tasks << task
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  my_list = List.new
+  puts "You have created a new list"
+end
+
+
+
+
+class Task
+  attr_reader :description
+  def initialize(description)
+    @description = description
+  end
 end
