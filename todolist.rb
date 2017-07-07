8db602772ad8a1c059facbbe624400c7dd395238
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-07 18:07:06 -0400

    Updates

--- todolist.rb
+++ todolist.rb
@@ -1,22 +1,18 @@
-class List
-  attr_reader :all_tasks
-  def initialize(all_tasks)
-    @all_tasks = []
+module Menu
+  def menu
+    puts "Welcome! Please choose from the following options:
+      1)  Add
+      2)  Show
+      3)  Quit"
   end
 
-  def add(task)
-    all_tasks << task
+  def show
+    menu
   end
-
 end
 
-if __FILE__ == $PROGRAM_NAME
-  my_list = List.new
-  puts "You have created a new list"
+module Promptable
 end
-
-
-
 
 class Task
   attr_reader :description
@@ -24,3 +20,30 @@ class Task
     @description = description
   end
 end
+
+class List
+  attr_reader :all_tasks
+  def initialize
+    @all_tasks = []
+  end
+
+  def add(task)
+    all_tasks << task
+  end
+
+  def show
+    all_tasks
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  my_list = List.new
+  puts 'You have created a new list'
+  #my_list.add(Task.new('Make Breakfast'))
+  #my_list.add(Task.new('Make dinner'))
+  my_list.add('Make Breakfast')
+  my_list.add('Make dinner')
+  puts 'You have added a task to the Todo List'
+  puts my_list.show
+end
+
